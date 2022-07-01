7d89d2314ad183bed530702463dd718b5b0a11c5
csherpa <chhoti0712@gmail.com>
2022-07-01 16:23:32 -0500

    import apiData and map the apiData

--- programs/src/components/Cards/index.js
+++ programs/src/components/Cards/index.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './styles.css';
+import { apiData } from '../../api';
 
 const Cards = () => {
+  const [cardInfo, setCardInfo] = useState([]);
+
+  const getCardInfo = () => {
+    apiData()
+      .then((data) => setCardInfo(data))
+      .catch((err) => console.log(err, 'getcardInfo Err'));
+  }
+  useEffect(() => {
+    getCardInfo();
+  }, [])
 
   return(
     <div>
       <h1>Card</h1>
+      <div>
+        {
+          cardInfo.map((val) => {
+            console.log(val, 'val');
+          })
+        }
+      </div>
     </div>
   );
 };
