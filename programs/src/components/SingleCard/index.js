e8fa4fac44613f7f59f2255d0d5a7a1d4e5c900f
csherpa <chhoti0712@gmail.com>
2022-07-02 16:58:51 -0500

    SingleCard with each programs info

--- programs/src/components/SingleCard/index.js
+++ programs/src/components/SingleCard/index.js
@@ -4,7 +4,16 @@ import './styles.css';
 const SingleCard = ({ info }) => {
   console.log(info, 'SingleCardInfo')
   return (
-    <div>SingleCard</div>
+    <div>
+    <div class="card">
+      <img src={info.image.cloudinaryURL} alt="Program Img" style={{width: '100%'}}/>
+      <div class="container">
+        <h4><b>Program Name:{info.name}</b></h4>
+        <p>Institution Name: {info.institution.name}</p>
+        <p>Program type: {info.programType}</p>
+      </div>
+    </div>
+    </div>
   )
 }
 
